8bfea9d1f889a22d918fea1704bf4d1896f59f50
Dom Hastings <dom111@users.noreply.github.com>
2021-01-17 21:13:24 +0000

    Initial commit

---
+++ Sail.d.ts
@@ -0,0 +1,5 @@
+import NavalTransport from '@civ-clone/base-unit-type-naval-transport/NavalTransport';
+export declare class Sail extends NavalTransport {
+  capacity(): number;
+}
+export default Sail;
